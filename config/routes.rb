feeb31c8dcaddbfc4afa48a6a4d185d74c738000
MFitzgerald89 <122069535+MFitzgerald89@users.noreply.github.com>
2023-05-09 22:31:36 -0400

    adds photos column to users table

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   post "/sessions" => "sessions#create"
 
   post "/users" => "users#create"
+  get "/users" => "users#index"
   get "/users/:id" => "users#show"
   patch "/users/:id" => "users#update"
   delete "/users/:id" => "users#destroy"
