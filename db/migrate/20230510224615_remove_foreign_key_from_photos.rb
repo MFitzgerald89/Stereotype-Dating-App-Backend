bda896bc0e93198b84349deb4565ea3dc15d225e
MFitzgerald89 <122069535+MFitzgerald89@users.noreply.github.com>
2023-05-18 16:29:15 -0400

    removes photos migration files

--- db/migrate/20230510224615_remove_foreign_key_from_photos.rb
+++
@@ -1,6 +0,0 @@
-class RemoveForeignKeyFromPhotos < ActiveRecord::Migration[7.0]
-  def change
-    remove_foreign_key :photos, :users
-
-  end
-end
--- db/migrate/20230514190742_drop_photos_table.rb
+++
@@ -1,5 +0,0 @@
-class DropPhotosTable < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :photos
-  end
-end
